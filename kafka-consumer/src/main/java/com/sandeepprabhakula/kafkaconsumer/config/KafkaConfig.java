12de60710314bc2839c421bcdf7868f9e995ad8e
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2025-08-17 12:32:02 +0530

    configured docker-compose (stable version)

--- kafka-consumer/src/main/java/com/sandeepprabhakula/kafkaconsumer/config/KafkaConfig.java
+++ kafka-consumer/src/main/java/com/sandeepprabhakula/kafkaconsumer/config/KafkaConfig.java
@@ -22,7 +22,7 @@ public class KafkaConfig {
     @Bean
     public ConsumerFactory<String, BusPayload> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, AppConstants.GROUP_ID);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
--- kafka-producer/src/main/java/com/sandeepprabhakula/kafkaproducer/config/KafkaConfig.java
+++ kafka-producer/src/main/java/com/sandeepprabhakula/kafkaproducer/config/KafkaConfig.java
@@ -34,7 +34,7 @@ public class KafkaConfig {
 
 
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "127.0.0.1:9092");
+                "kafka:9092");
         config.put(
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);
